7db424a7308d5327ea26f9bf1b72375256c07d59
hezhijun <zju.hzj@gmail.com>
2014-02-27 23:50:56 +0800

    phonegap build test

---
+++ js/todo.js
@@ -0,0 +1,15 @@
+﻿function todoCtrl($scope){
+	$scope.todos = [];
+	$scope.addTodo = function(todoText){
+		var todo = {
+			text: todoText,
+			done: false
+		}
+		$scope.todos.push(todo);
+		$scope.todoText = null;
+	}
+	
+	$scope.removeTodo = function(i){
+		$scope.todos.splice(i,1);
+	}
+}
